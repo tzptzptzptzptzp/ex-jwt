33debfee17c2cd9619b69dfbbc097f67fa20ea58
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-29 23:41:52 +0900

    ✨ Registerポストメソッドを作成

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,11 @@ router.get("/", (req, res) => {
   res.send("Auth Route");
 });
 
+router.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(email, password);
+});
+
 module.exports = router;
