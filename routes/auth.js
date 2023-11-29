fc812fa178cfd74a9064e496494343683c6707ce
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-29 23:44:04 +0900

    🦺 バリデーションを追加

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
+const { body, validationResult } = require("express-validator");
 
 router.get("/", (req, res) => {
   res.send("Auth Route");
 });
 
-router.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  console.log(email, password);
-});
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
