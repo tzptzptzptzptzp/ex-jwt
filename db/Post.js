4cf41be04599f49e7d9ebe3eb5789eeb1d78ddcc
tzptzptzptzptzp <tzp2539@gmail.com>
2023-12-01 23:41:29 +0900

    🗃️ 投稿のモックデータを作成

---
+++ db/Post.js
@@ -0,0 +1,23 @@
+const publicPosts = [
+  {
+    title: "Public Post",
+    body: "This is a public post",
+  },
+  {
+    title: "Public Post",
+    body: "This is a public post",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "Private Post",
+    body: "This is a private post",
+  },
+  {
+    title: "Private Post",
+    body: "This is a private post",
+  },
+];
+
+module.exports = { publicPosts, privatePosts };
