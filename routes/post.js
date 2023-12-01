ea8b4a9579700c8edc4dd62232ecc5c3035f6133
tzptzptzptzptzp <tzp2539@gmail.com>
2023-12-02 00:07:24 +0900

    ✨ 会員記事取得APIを実装

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
-const jwt = require("jsonwebtoken");
+const checkJWT = require("../middleware/checkJWT");
 
 const { publicPosts, privatePosts } = require("../db/Post");
 
 router.get("/public", (req, res) => {
   res.json(publicPosts);
+});
+
+router.get("/private", checkJWT, (req, res) => {
+  res.json(privatePosts);
 });
 
 module.exports = router;
